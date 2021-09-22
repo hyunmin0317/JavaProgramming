ed4f0c7bd8322dd671e25396d85316168f917fcd
hyunmin0317 <choihm9903@naver.com>
2021-09-22 15:00:44 +0900

    Update week04

--- 2021-2/src/exercises/week04/Frame.java
+++ 2021-2/src/exercises/week04/Frame.java
@@ -1,11 +1,17 @@
 package exercises.week04;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Frame {
 	public static void main(String[] args) {
 		JFrame f = new JFrame();
 		f.setSize(600, 400);
+		
+		Container pane = f.getContentPane();
+		JButton b = new JButton("hello");
+		pane.add(b);
+		
 		f.setVisible(true);
 		f.setTitle("Main");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
