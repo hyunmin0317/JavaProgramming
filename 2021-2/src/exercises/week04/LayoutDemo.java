a4c19a9877de68264cefddd0d8724258bb4f07a1
hyunmin0317 <choihm9903@naver.com>
2021-09-22 15:46:19 +0900

    Update week04

---
+++ 2021-2/src/exercises/week04/LayoutDemo.java
@@ -0,0 +1,29 @@
+package exercises.week04;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LayoutDemo {
+	public static void main(String[] args) {
+		JFrame f = new JFrame("Window");
+		
+		JButton b1 = new JButton("hello 1");
+		b1.setPreferredSize(new Dimension(1000, 100));
+		b1.setMinimumSize(new Dimension(600, 100));
+		b1.setMaximumSize(new Dimension(1200, 100));
+		
+		JButton b2 = new JButton("hello 2");
+		b2.setPreferredSize(new Dimension(1000, 100));
+		b2.setMinimumSize(new Dimension(600, 100));
+		b2.setMaximumSize(new Dimension(1200, 100));
+		
+		JPanel p = new JPanel();
+		p.setPreferredSize(new Dimension(1000, 3000));
+		p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+		p.add(b1);	p.add(b2);
+		
+		f.getContentPane().add(p);
+		f.pack();
+		f.setVisible(true);
+	}
+}
