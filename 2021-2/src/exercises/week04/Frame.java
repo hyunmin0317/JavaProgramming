d5da3c686cfc9503f04374e697ddd8d1e7f4608c
hyunmin0317 <choihm9903@naver.com>
2021-09-22 14:57:03 +0900

    Update week04

---
+++ 2021-2/src/exercises/week04/Frame.java
@@ -0,0 +1,13 @@
+package exercises.week04;
+
+import javax.swing.JFrame;
+
+public class Frame {
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.setSize(600, 400);
+		f.setVisible(true);
+		f.setTitle("Main");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
