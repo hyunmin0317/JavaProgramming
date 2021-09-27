355f784d1dbcf89c707056ef7d5a996079f8d6f4
hyunmin0317 <choihm9903@naver.com>
2021-09-27 13:00:27 +0900

    Update Observer-ArrayList

--- 2021-2/src/exercises/week04/WeatherData.java
+++ 2021-2/src/exercises/week04/WeatherData.java
@@ -14,7 +14,7 @@ public class WeatherData implements Subject {
 	
 	public void registerObserver(Observer o) {
 		observers.add(o);
-		}
+	}
 	
 	public void removeObserver(Observer o) {
 		int i = observers.indexOf(o);
@@ -26,34 +26,31 @@ public class WeatherData implements Subject {
 	@Override
 	public void notifyObservers() {
 		for (int i = 0; i < observers.size(); i++) {
-			Observer observer = 
-			(Observer) observers.get(i);
-			observer.update(temperature, humidity, 
-			pressure);
-			}
+			Observer observer = (Observer) observers.get(i);
+			observer.update(temperature, humidity, pressure);
+		}
 	}
 	
 	public void measurementsChanged() {
 		notifyObservers();
-		}
+	}
 	
-
 	public void setMeasurements(float temperature, float humidity, float pressure) {
 		this.temperature = temperature;
 		this.humidity = humidity;
 		this.pressure = pressure;
 		measurementsChanged();
-		}
+	}
 	
 	public float getTemperature() {
 		return temperature;
-		}
+	}
 	
 	public float getHumidity() {
 		return humidity;
-		}
+	}
 	
 	public float getPressure() {
 		return pressure;
-		}
+	}
 }
