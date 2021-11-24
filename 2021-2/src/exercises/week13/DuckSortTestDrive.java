2571513dd4d981de5d66d4e2583867a51649be37
hyunmin0317 <choihm9903@naver.com>
2021-11-24 16:20:05 +0900

    Update week13

--- 2021-2/src/exercises/week13/DuckSortTestDrive.java
+++ 2021-2/src/exercises/week13/DuckSortTestDrive.java
@@ -2,17 +2,17 @@ package exercises.week13;
 
 public class DuckSortTestDrive {
 	public static void main(String[] args) {
-	Duck[] ducks = { 
-		new Duck("Daffy", 8), new Duck("Dewey", 2),
-		new Duck("Howard", 7), new Duck("Louie", 2),
-		new Duck("Donald", 10), new Duck("Huey", 2)
-	};
-	System.out.println("정렬 전:");
-	display(ducks);
-	Arrays.sort(ducks);
-	
-	System.out.println("\nAfter sorting: ");
-	display(ducks);
+		Duck[] ducks = { 
+			new Duck("Daffy", 8), new Duck("Dewey", 2),
+			new Duck("Howard", 7), new Duck("Louie", 2),
+			new Duck("Donald", 10), new Duck("Huey", 2)
+		};
+		System.out.println("정렬 전:");
+		display(ducks);
+		Arrays.sort(ducks);
+		
+		System.out.println("\nAfter sorting: ");
+		display(ducks);
 	}
 	
 	public static void display(Duck[] ducks) {
