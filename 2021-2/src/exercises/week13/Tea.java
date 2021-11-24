a1c63e7280fa2f42058fcb765ed76e1821848200
hyunmin0317 <choihm9903@naver.com>
2021-11-24 15:38:36 +0900

    Update week13

---
+++ 2021-2/src/exercises/week13/Tea.java
@@ -0,0 +1,11 @@
+package exercises.week13;
+
+class Tea extends CaffeineBeverage {
+	public void brew() {
+		System.out.println("차를 우려내는 중");
+	}
+	
+	public void addCondiments() {
+		System.out.println("레몬을 추가하는 중");
+	}
+}
