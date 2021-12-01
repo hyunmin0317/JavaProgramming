8fc55f63e15f7e1da2487f629568c23905002110
hyunmin0317 <choihm9903@naver.com>
2021-12-01 11:53:53 +0900

    Update answer10

--- 2021-2/src/answers/assignment10/NameSorter.java
+++ 2021-2/src/answers/assignment10/NameSorter.java
@@ -1,17 +1,12 @@
 package answers.assignment10;
 
-public class NameSorter {
-
+public class NameSorter extends BubbleSorter{
 	public void bubbleSort(Person[] data) {
-        for (int i = 0; i < data.length - 1; i++) {
-            for (int j = 0; j < data.length - 1; j++) {
-                if (data[j].getName().compareTo(data[j + 1].getName()) > 0) {
-                	Person temp = data[j];
-                    data[j] = data[j + 1];
-                    data[j + 1] = temp;
-                }
-            }
-        }
+        sort(data);
     }
-
+	
+	@Override
+	public int compareTo(Person a, Person b) {
+		return a.getName().compareTo(b.getName());
+	}
 }
