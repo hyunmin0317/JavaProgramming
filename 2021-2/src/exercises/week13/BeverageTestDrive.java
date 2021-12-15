c1fc5957d0ecf641b043b21d35908352a0134cd8
hyunmin0317 <choihm9903@naver.com>
2021-12-15 11:25:24 +0900

    Update week13

---
+++ 2021-2/src/exercises/week13/BeverageTestDrive.java
@@ -0,0 +1,9 @@
+package exercises.week13;
+
+public class BeverageTestDrive {
+	public static void main(String[] args) {
+		CoffeeWithHook coffeeHook = new CoffeeWithHook();
+		System.out.println("\n커피 준비중…");
+		coffeeHook.prepareRecipe();
+	}
+}
